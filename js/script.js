7e4b75d47d8fb4dce0a411d06b0062c33689defa
[Lindsey Dukles] <[lindseydukles@gmail.com]>
2022-05-09 16:34:00 -0400

    small changes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 var settings = {
-    "url": "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "X-eBirdApiToken": "g239pf3095hj"
-    },
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-//   Variables
-const URL = "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full"
+  url: "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full",
+  method: "GET",
+  timeout: 0,
+  headers: {
+    "X-eBirdApiToken": "g239pf3095hj",
+  },
+};
 
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+//   Variables
+const URL =
+  "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full";
 
 // Element References
 const $comName = $("#comName");
@@ -20,7 +20,7 @@ const $sciName = $("#sciName");
 const $locName = $("#locName");
 const $obsDt = $("#obsDt");
 const $howMany = $("#howMany");
-const $obsValid =$("obsValid");
+const $obsValid = $("obsValid");
 const $input = $(`input[type=text]`);
 const $form = $("form");
 
@@ -29,25 +29,25 @@ $form.on("submit", handleGetData);
 
 // Functions
 function handleGetData(event) {
-    event.preventDefault();
-// data expansion to indlude form submissions
-const userInput = $input.val();
+  event.preventDefault();
+  // data expansion to indlude form submissions
+  const userInput = $input.val();
 
-$.ajax(URL + userInput).then(
+  $.ajax(URL + userInput).then(
     function (data) {
-        console.log("Rare Birds Detected")
-        $comName.text(data.comName);
-$sciName.text(data.sciName);
-$locName.text(data.locName);
-$obsDt.text(data.obsDt);
-$howMany.text(data.howMany);
-$obsValid.text(data.obsValid);
+      console.log("Rare Birds Detected");
+      $comName.text(data.comName);
+      $sciName.text(data.sciName);
+      $locName.text(data.locName);
+      $obsDt.text(data.obsDt);
+      $howMany.text(data.howMany);
+      $obsValid.text(data.obsValid);
     },
     function (error) {
-        console.log("something ain't right");
-        console.log(error);
+      console.log("something ain't right");
+      console.log(error);
     }
-);
+  );
 }
 
 //   All elements
