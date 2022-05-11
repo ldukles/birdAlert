1485157335e18dff6623f2470e6cc44901e4dc37
[Lindsey Dukles] <[lindseydukles@gmail.com]>
2022-05-11 13:36:04 -0400

    template literals update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-var $p = $('p');
-$p.hide();
+// Hide Elements until prompted
+// var $p = $("p");
+// $p.hide();
 
+
+// API Settings
 var settings = {
-  "url": "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full",
+  "url": "https://api.ebird.org/v2/data/obs/{{regionCode}}/recent/notable?detail=full",
   "method": "GET",
   "timeout": 0,
-  "data": {back: 1, detail: "simple", maxResults: 10},
+  "data": { back: 1, maxResults: 10 },
   "headers": {
-    "X-eBirdApiToken": "g239pf3095hj"
+    "X-eBirdApiToken": "g239pf3095hj",
   },
 };
 
+// Checking API Connection
 // $.ajax(settings).done(function (response) {
 //   console.log(response);
 // });
+
+
 //   Variables
-const URL = "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full"
+
 
 
 // Element References
@@ -24,41 +30,45 @@ const $sciName = $("#sciName");
 const $locName = $("#locName");
 const $obsDt = $("#obsDt");
 const $howMany = $("#howMany");
-const $obsValid =$("obsValid");
+const $obsValid = $("#obsValid");
 const $input = $(`input[type=text]`);
 const $form = $("form");
 
+
 // Event Listeners
-$form.on("go", handleGetData);
+$form.on("submit", handleGetData);
+
 
 // Functions
 function handleGetData(event) {
   event.preventDefault();
-// data expansion to indlude form submissions
-const userInput = $input.val();
+  // data expansion to include form submissions
+  const URL =
+  `https://api.ebird.org/v2/data/obs/US-` + `${userInput}` + `/recent/notable?detail=simple`;
+  const userInput = $input.val();
 
-$.ajax(URL + userInput).then(
-  function (data) {
-      console.log("Rare Birds Detected")
-      $comName.text(data.comName);
-$sciName.text(data.sciName);
-$locName.text(data.locName);
-$obsDt.text(data.obsDt);
-$howMany.text(data.howMany);
-$obsValid.text(data.obsValid);
-  },
-  function (error) {
+  $.ajax(URL).then(
+    function (data) {
+      console.log("rare birds dettected");
+      $comName.text(data.Common);
+      $sciName.text(data.Scientific);
+      $locName.text(data.Location);
+      $obsDt.text(data.Date);
+      $howMany.text(data.How);
+      $obsValid.text(data.Verified);
+    },
+    function (error) {
       console.log("something ain't right");
       console.log(error);
-  }
-);
+    }
+  );
 }
 
-function render() {
-  $comName.text(birdData.comName);
-  $sciName.text(birdData.sciName);
-  $locName.text(birdData.locName);
-  $obsDt.text(birdData.obsDt);
-  $howMany.text(birdData.howMany);
-  $obsValid.text(birdData.obsValid);
-};
+// function render() {
+//   $comName.text(birdData.comName);
+//   $sciName.text(birdData.sciName);
+//   $locName.text(birdData.locName);
+//   $obsDt.text(birdData.obsDt);
+//   $howMany.text(birdData.howMany);
+//   $obsValid.text(birdData.obsValid);
+// }
