85b104ba0568106554eaf76cb25d38dc5efef5f9
[Lindsey Dukles] <[lindseydukles@gmail.com]>
2022-05-11 09:29:06 -0400

    hard js reset

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// var URL = {
-//   url: "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full",
-//   method: "GET",
-//   timeout: 0,
-//   data: { back: 1, detail: "simple", maxResults: 10 },
-//   headers: {
-//     "X-eBirdApiToken": "g239pf3095hj",
-//   },
-// };
+var settings = {
+  "url": "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full",
+  "method": "GET",
+  "timeout": 0,
+  "data": {back: 1, detail: "simple", maxResults: 10},
+  "headers": {
+    "X-eBirdApiToken": "g239pf3095hj"
+  },
+};
 
-// $.ajax(URL).done(function (response) {
-//   console.log(response);
-// });
-// Variables
-// const URL =
-//   "https://api.ebird.org/v2/data/obs/'${userInput}'/recent/notable?detail=full";
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+//   Variables
+const URL = "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full"
+
 
 // Element References
 const $comName = $("#comName");
@@ -21,7 +21,7 @@ const $sciName = $("#sciName");
 const $locName = $("#locName");
 const $obsDt = $("#obsDt");
 const $howMany = $("#howMany");
-const $obsValid = $("obsValid");
+const $obsValid =$("obsValid");
 const $input = $(`input[type=text]`);
 const $form = $("form");
 
@@ -31,75 +31,22 @@ $form.on("submit", handleGetData);
 // Functions
 function handleGetData(event) {
   event.preventDefault();
-  // data expansion to indlude form submissions
-  const userInput = $input.val();
+// data expansion to indlude form submissions
+const userInput = $input.val();
 
-  $.ajax(URL + userInput).then(
-    function (data) {
-      console.log("Rare Birds Detected");
+$.ajax(URL + userInput).then(
+  function (data) {
+      console.log("Rare Birds Detected")
       $comName.text(data.comName);
-      $sciName.text(data.sciName);
-      $locName.text(data.locName);
-      $obsDt.text(data.obsDt);
-      $howMany.text(data.howMany);
-      $obsValid.text(data.obsValid);
-    },
-    function (error) {
+$sciName.text(data.sciName);
+$locName.text(data.locName);
+$obsDt.text(data.obsDt);
+$howMany.text(data.howMany);
+$obsValid.text(data.obsValid);
+  },
+  function (error) {
       console.log("something ain't right");
       console.log(error);
-    }
-  );
+  }
+);
 }
-function render() {
-  $comName.text(birdData.comName);
-  $sciName.text(birdData.sciName);
-  $locName.text(birdData.locName);
-  $obsDt.text(birdData.obsDt);
-  $howMany.text(birdData.howMany);
-  $obsValid.text(birdData.obsValid);
-}
-
-var URL = {
-  url: "https://api.ebird.org/v2/data/obs/US-NY/recent/notable?detail=full",
-  method: "GET",
-  timeout: 0,
-  data: { back: 1, detail: "simple", maxResults: 10 },
-  headers: {
-    "X-eBirdApiToken": "g239pf3095hj",
-  },
-};
-
-// $.ajax(URL).done(function (response) {
-//   console.log(response);
-// });
-//   All elements
-//   {
-//     "speciesCode": "libher",
-//     "comName": "Little Blue Heron",
-//     "sciName": "Egretta caerulea",
-//     "locId": "L295658",
-//     "locName": "Wallkill River NWR--Liberty Marsh (NY)",
-//     "obsDt": "2017-08-23 10:11",
-//     "howMany": 1,
-//     "lat": 41.2833266,
-//     "lng": -74.5262526,
-//     "obsValid": false,
-//     "obsReviewed": false,
-//     "locationPrivate": false,
-//     "subnational2Code": "US-NY-071",
-//     "subnational2Name": "Orange",
-//     "subnational1Code": "US-NY",
-//     "subnational1Name": "New York",
-//     "countryCode": "US",
-//     "countryName": "United States",
-//     "userDisplayName": "Kathleen Ashman",
-//     "subId": "S38783126",
-//     "obsId": "OBS527233428",
-//     "checklistId": "CL22364",
-//     "presenceNoted": false,
-//     "hasComments": false,
-//     "firstName": "Kathleen",
-//     "lastName": "Astahman",
-//     "hasRichMedia": false,
-//     "locID": "L295658"
-//   },
